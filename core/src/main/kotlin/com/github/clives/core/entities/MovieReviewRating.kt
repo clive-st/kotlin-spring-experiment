d424ea7e8e7c61d920a53bc9a5227e9b4dfdc4a9
sosthene <Clive.St@protonmail.com>
2022-02-15 17:28:36 -0300

    jira-xxx : review model update

--- core/src/main/kotlin/com/github/clives/core/entities/MovieReviewRating.kt
+++ core/src/main/kotlin/com/github/clives/core/entities/MovieReviewRating.kt
@@ -1,15 +1,16 @@
 package com.github.clives.core.entities
 
 data class MovieReviewRating (
+        val imdb: Imdb,
     val rating: Rating,
     val userName: String
 )
 
-data class Rating (val nbrStarval :Int){
+data class Rating (val star :Int){
     companion object {
-        operator fun invoke(rating: Int): Rating {
-            //todo validation
-            return Rating(rating)
+        operator fun invoke(star: Int): Rating {
+            //todo validation (1 to 5)
+            return Rating(star)
         }
     }
 }
